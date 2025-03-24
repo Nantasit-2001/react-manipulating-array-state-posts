ce83091d2c5d82f826c0499824dbe0c6c0de23e1
nantasit <nantasitnsboss@gmail.com>
2025-03-24 14:49:09 +0700

    feat:function plusLike and deleteLike and use map to render post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,41 @@
+import { useState } from "react";
+import { postData } from "../raw-data/post-data";
+
 function Posts() {
+  const [dataInPost,setdataInPost] = useState(postData);
+  
+  function addLikes (index) {
+    const tempdeta =[...postData]
+    tempdeta[index].likes +=1 
+    setdataInPost(tempdeta)
+  }
+  function deleteLike (index){
+    const tempdeta =[...postData]
+    tempdeta[index].likes -=1 
+    setdataInPost(tempdeta)
+  }
+
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
-            </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
+      {dataInPost.map((data,index)=> 
+              <div class="post-item" key={data.title}> 
+                <div class="post-header">
+                  <h2>{data.title}</h2>
+                  <div class="post-social-media-stats">
+                  <span class="stats-topic">Likes: </span>
+                  <span class="post-likes">{data.likes}</span>
+                </div>
+              </div>
+              <p class="post-content">
+            {data.content}
           </p>
           <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
+            <button class="like-button"onClick={()=>addLikes(index)} >Like</button>
+            <button class="dislike-button" onClick={data.likes > 0 ? () => deleteLike(index) :()=>alert(data.title+" can't Dislike") }>Dislike</button>
           </div>
-        </div>
+        </div>)}
       </div>
     </div>
   );
