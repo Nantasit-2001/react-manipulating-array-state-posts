49c11b203e4cb3d4bee4fdd9194f529856b5ec9f
nantasit <nantasitnsboss@gmail.com>
2025-03-24 17:45:59 +0700

    refactor:change add-delete with index to id(use.map)

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,22 +4,28 @@ import { postData } from "../raw-data/post-data";
 function Posts() {
   const [dataInPost,setdataInPost] = useState(postData);
   
-  function addLikes (index) {
-    const tempdeta =[...postData]
-    tempdeta[index].likes +=1 
-    setdataInPost(tempdeta)
+  function addLikes (postId) {
+    // const tempdeta =[...postData]
+    // tempdeta[index].likes +=1 
+    // setdataInPost(tempdeta)
+    const tempdeta = dataInPost.map(post=>
+      (post.id === postId)? {...post,likes:post.likes+1}:post)
+      setdataInPost(tempdeta)
   }
-  function deleteLike (index){
-    const tempdeta =[...postData]
-    tempdeta[index].likes -=1 
-    setdataInPost(tempdeta)
+  function deleteLike (postId){
+    // const tempdeta =[...postData]
+    // tempdeta[index].likes -=1 
+    // setdataInPost(tempdeta)  
+    const tempdeta = dataInPost.map(post=>
+      (post.id === postId)? {...post,likes:post.likes-1}:post)
+      setdataInPost(tempdeta)
   }
 
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-      {dataInPost.map((data,index)=> 
+      {dataInPost.map((data)=> 
               <div class="post-item" key={data.title}> 
                 <div class="post-header">
                   <h2>{data.title}</h2>
@@ -32,8 +38,8 @@ function Posts() {
             {data.content}
           </p>
           <div class="post-actions">
-            <button class="like-button"onClick={()=>addLikes(index)} >Like</button>
-            <button class="dislike-button" onClick={data.likes > 0 ? () => deleteLike(index) :()=>alert(data.title+" can't Dislike") }>Dislike</button>
+            <button class="like-button"onClick={()=>addLikes(data.id)}>Like</button>
+            <button class="dislike-button" onClick={data.likes > 0 ? () => deleteLike(data.id) :()=>alert(data.title+" can't Dislike") }>Dislike</button>
           </div>
         </div>)}
       </div>
